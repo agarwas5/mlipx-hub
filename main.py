e3204388b60ea07a4588e4293be6ab0fc8b414aa
ShaunHan <hanshuangshiren@gmail.com>
2025-07-15 15:42:55 +0000

    adding pourbaix example

---
+++ main.py
@@ -0,0 +1,21 @@
+import mlipx
+import zntrack
+
+from models import MODELS
+
+project = zntrack.Project()
+
+frames = []
+
+with project.group("initialize"):
+    for path in ['./LiCoO_schmidt_2021_mad_recomputed.xyz']:
+        frames.append(mlipx.LoadDataFile(path=path))
+
+
+for model_name, model in MODELS.items():
+    for idx, data in enumerate(frames):
+        with project.group(model_name, str(idx)):
+            pd = mlipx.PourbaixDiagram(data=data.frames, model=model, pH=0, V=1.6)
+
+
+project.build()
